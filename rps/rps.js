889d7d3875cc0a356b38154676bffadaef1250bc
dales <dalesimmons0210@gmail.com>
2021-06-11 23:30:35 -0700

    Updated

--- rps/rps.js
+++ rps/rps.js
@@ -1,58 +1,95 @@
-let wins = 0;
-let losses = 0;
-let ties = 0;
+let userScore = 0;
+let compScore = 0;
+const userScore_span = document.getElementById("playerScore");
+const compScore_span = document.getElementById("compScore");
+const scoreboard_div = document.querySelector(".scoreboard");
+const result_p = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissor_div = document.getElementById("s");
+rock_div.addEventListener('click', game('r')); 
+   
+    
+    paper_div.addEventListener('click', game('p'));
+    
+    
+    scissor_div.addEventListener('click', game('s'));
+    
+//main();
 
-function runGame() {
-    getPlay();
-    getComputerMove();
+/*
+function main() {
+    rock_div.addEventListener('click', game('r')); 
+   
+    
+    paper_div.addEventListener('click', game('p'));
+    
+    
+    scissor_div.addEventListener('click', game('s'));
     
 }
-
-function getPlay() {
-    let play = prompt("What is your play?");
-    if (play === null) {
-        alert("Please close the tab");
-    }
-    play = play.toLowerCase();
+*/
+function game(userChoice) {
+    let compChoice = getCompChoice();
+    console.log("comp choice is: " + compChoice + " userchoice is " + userChoice);
     
-    switch(play) {
-        case "rock":
-            play === "rock";
+    switch (userChoice + compChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            console.log("User won");
+            win(userChoice, compChoice)
             break;
-        case "paper":
-            play === "paper";
+        case "rp":
+        case "ps":
+        case "sr":
+            console.log("User lost");
+            lose(userChoice, compChoice);
             break;
-        case "scissors":
-            play === "scissors";
+        case "rr":
+        case "pp":
+        case "ss":
+            console.log("Tie game");
+            tie(userChoice, compChoice);
             break;
     }
-    return play;
-}
-function getComputerMove() {
-    let num = Math.random();
-    let compMove = "";
-    if (num <= 0.34) {
-        compMove === "paper";
-    } else if (num <= 0.67) {
-        compMove === "rock";
-    } else {
-        compMove === "scissors"
-    }
-    return compMove;
 }
 
-function calculate(play, compMove) {
-    if ((play === "scissors" && compMove === "scissors") || (play === "paper" && compMove === "paper") || (play === "rock" && compMove === "rock")) {
-        alert("Tie game!");
-        ties++;
-        return ties;
-    } else if ((play === "scissors" && compMove === "paper") || (play === "paper" && compMove === "rock") || (play === "rock" && compMove === "scissors")) {
-        alert("You win!");
-        wins++;
-        return wins;
-    } else {
-        alert("You lost!");
-        losses++;
-        return losses;
-    }   
+function getCompChoice() {
+    const choices = ['r', 'p', 's'];
+    const pick = Math.floor(Math.random() * 3);
+    console.log("CHOICE IS" + pick);
+    return choices[pick];
+}
+
+function win(userChoice, compChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_p.innerHTML = convertWord(userChoice) + " beats " + convertWord(compChoice) + ". You win!";
+    document.getElementById(userChoice).classList.add('greenglow');
+    setTimeout(document.getElementById(userChoice).classList.remove('greenglow'), 300);
+}
+
+function lose(userChoice, compChoice) {
+    compScore++;
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_p.innerHTML = convertWord(userChoice) + " loses to " + convertWord(compChoice) + ". You lose!";
+    document.getElementById(userChoice).classList.add('redglow');
+    setTimeout(document.getElementById(userChoice).classList.remove('redglow'), 300);
+}
+
+function tie(userChoice, compChoice) {
+    userScore_span.innerHTML = userScore;
+    compScore_span.innerHTML = compScore;
+    result_p.innerHTML = convertWord(userChoice) + " equals " + convertWord(compChoice) + ". It's a tie!";
+    document.getElementById(userChoice).classList.add('greyglow');
+    setTimeout(document.getElementById(userChoice).classList.remove('greyglow'), 300);
+}
+
+function convertWord(letter) {
+    if (letter === "r") return "Rock";
+    if (letter === "p") return "Paper";
+    return "Scissors";
 }
